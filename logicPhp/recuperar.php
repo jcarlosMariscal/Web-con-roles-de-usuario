02553a6be613082f866af17d5c3988d7b1def7a9
Karl16Animelos20 <juanc.ramirezm16@gmail.com>
2022-04-28 00:02:12 -0500

    security

--- logicPhp/recuperar.php
+++ logicPhp/recuperar.php
@@ -40,13 +40,13 @@ if(!empty($_POST)){
                         $mail->isSMTP();                                            //Send using SMTP
                         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
                         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-                        $mail->Username   = 'cuenta0test61@gmail.com';                     //SMTP username
-                        $mail->Password   = 'cuenta0Test61Master';                               //SMTP password
+                        $mail->Username   = 'example@gmail.com';                     //SMTP username
+                        $mail->Password   = 'password';                               //SMTP password
                         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
                         $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
                         //Recipients
-                        $mail->setFrom('cuenta0test61@gmail.com', 'Sistema Ventas');
+                        $mail->setFrom('example@gmail.com', 'Sistema Ventas');
                         $mail->addAddress($correo, $result['nombre']);     //Add a recipient
                         // $mail->addAddress($correo, 'Joe User');     //Add a recipient
                         //Content
